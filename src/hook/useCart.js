08a59eada2400e903eb75fac91e662bf7ed93073
Hossein Rostami <hossein.rostami9394@gmail.com>
2025-01-25 00:13:37 +0330

    Initail commit

---
+++ src/hook/useCart.js
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react"
+
+export const useCart =()=>{
+    const [cartItems , setCartItems] = useState();
+
+    useEffect(()=>{
+        const data = localStorage.getItem("HosseinsShop_cart")
+        setCartItems(!!JSON.parse(data) ? JSON.parse(data) : [])
+    }
+    ,[])
+    useEffect(()=>{
+        if( cartItems !== undefined)
+        localStorage.setItem("HosseinsShop_cart" , JSON.stringify
+        (cartItems))
+    },[cartItems])
+
+
+    const addToCart =(itemId)=>{
+        if( !cartItems?.find((item)=> item.id === itemId))
+        setCartItems([...cartItems , {id : itemId , count : 1} ])
+        else
+        setCartItems(cartItems.map((item)=>{
+            if( item.id=== itemId)
+            return {...item , count : item.count + 1}    
+            else return item
+        }))
+    }
+
+    const removeFromCart =(itemId) =>{
+        setCartItems(cartItems.map ((i)=>{
+            if(i.id ===itemId)
+            return {...i , count : i.count ===0 ? 0: i.count - 1}   
+            else return i
+        }))
+    }
+
+    const resetCart =()=>{
+        setCartItems([]);
+        localStorage.removeItem("HosseinsShop_cart")
+        localStorage.clear()
+    }
+    return {cartItems , addToCart , removeFromCart , resetCart}
+}
